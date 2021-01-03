2eab32ca0c75222e19343411244efe7a3f9f0e51
xCodiq <elmarblume91@gmail.com>
2021-01-03 19:57:25 +0100

    v1.0.0: Updated license

--- src/main/java/gg/xcodiq/library/gui/GUIWorker.java
+++ src/main/java/gg/xcodiq/library/gui/GUIWorker.java
@@ -1,7 +1,7 @@
 /*
  * Licensed under the MIT License
  *
- * Copyright (c) 2021 xCodiq (Elmar B.)
+ * Copyright (c) 2021 Pixel Entertainment LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
